35af8bcabaad6b175ce3e0f0be5aa24dd0a53c10
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2021-07-09 17:18:12 +0100

    Updated

---
+++ js/app.js
@@ -0,0 +1,46 @@
+// GETTING THE INPUT VALUE
+const buttonContainer = document.querySelector('.button-container');
+buttonContainer.addEventListener('click', function () {
+    const inputText = document.getElementById('filter-jobs').value;
+    console.log(inputText);
+});
+
+// GETTING THE DATA FROM THE JSON FILE AND PUTTING THEM IN THE WEBPAGE
+function getJobs() {
+    return fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        return data;
+    });
+}
+
+function showJobs(jobs) {
+    let jobsContainer = document.querySelector('.jobs-container');
+    let jobsHTML = '';
+
+    jobs.forEach((job) => {
+        jobsHTML += `
+            <div class="job-tile">
+                <div class="top">
+                    <img src="${job.logo}" alt="">
+                    <span class="material-icons more_horiz">more_horiz</span>
+                </div>
+                <div class="rolename">
+                    <span>${job.roleName}</span>
+                </div>
+                <div class="description">
+                    <span>${job.requirements.content}</span>
+                </div>
+                <div class="buttons">
+                    <div class="button apply-now">Apply Now</div>
+                    <div class="button">Message</div>
+                </div>
+            </div>
+        `
+    });
+    jobsContainer.innerHTML = jobsHTML;
+}
+
+getJobs().then(data => {
+    showJobs(data);
+});
